09a73dadbdd1ea4b4cbc8e87fa241bf484c1e6f7
9dotslearning <9dotslearning@gmail.com>
2022-05-25 05:46:19 -0400

    styles + lesson 2 mostly complete

--- script.js
+++ script.js
@@ -1,26 +1,13 @@
-document.getElementById('tst').style.color = 'red';
-let tst = document.getElementById('tst').innerHTML;
-console.log('tst: ' + tst);
-
-function tstclick() {
-  alert('got here!');
-}
-
-let testcode = "data:text/html;charset=utf-8,%3Chtml%3E%3Cbody%3Efoo%3C/body%3E%3C/html%3E";
-
-let mycode  = document.getElementById('mycode').innerHTML;
-console.log(mycode);
-
-// var html = document.getElementById("html").innerHTML;
-// var dataURI = 'data:text/html,' + encodeURIComponent(html);
-
-// from http://jsfiddle.net/TCnG4/
-// let dataURI = 'data:text/html;charset=utf-8,' + encodeURIComponent(mycode);
 /*
-let dataURI = testcode;
-let converted = encodeURIComponent(mycode);
-console.log('converted: ' + converted);
-let first = document.getElementById('01_first');
-first.setAttribute('data-sketch-src', dataURI);
-console.log('dataUri: ' + dataURI);
+let test = document.getElementById('test');
+console.log('test: ' + test);
+
+
+let answers = document.getElementsByClassName('showAnswers');
+console.log('answers: ' + answers[0].innerHTML);
+console.log('hello');
+
+$(document).ready( function() {
+alert('Example of a basic alert box in jquery', 'jquery basic alert box');
+});
 */
